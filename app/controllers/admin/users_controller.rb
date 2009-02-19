b9a2d0409b7ceec3697a44b1900850d5e8bd41ab
saberma <mahb45@gmail.com>
2009-02-19 08:32:34 +0800

    remove active_scaffold plugin

--- app/controllers/admin/users_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::UsersController < ApplicationController
-  layout 'admin'
-  active_scaffold :user do |config|
-    config.actions = [:list, :search]
-    config.columns = [:login, :email, :created_at, :last_login, :questions_count, :answers_count]
-  end
-end
