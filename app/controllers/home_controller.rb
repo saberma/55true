4eb4412e10b7dac21c12e7ddf01a7a386df30685
saberma <mahb45@gmail.com>
2009-06-13 15:42:55 +0800

    首页增加网站作用说明

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,14 @@
 class HomeController < ApplicationController
   def index
-    
     #用户问的
     if logged_in?
       @user_unanswer_question_list = Question.unanswer.of current_user
       @user_answered_question_list = Question.limit(2).answered.of(current_user)
       @question_list = Question.answered.not_belong_to(current_user).paginate(:page => params[:page])
+      flash.now[:notice] = "这里的#{User.all.size}个朋友(共玩了#{@question_list.total_entries}次)，点击下一行“我敢玩”继续吧!" unless flash[:notice]
     else
       @question_list = Question.answered.paginate(:page => params[:page])
-      flash.now[:notice] = "这里有#{User.all.size}个朋友，想跟你一起玩真心话，点击下一行“我敢玩”就可以开始了!"
+      flash.now[:notice] = "这里的#{User.all.size}个朋友(共玩了#{@question_list.total_entries}次)，想跟你一起玩真心话，点击下一行“我敢玩”就可以开始了!"
     end
   end
 
