60397a980e67f3259e767541c97f3c74c4e5bc4c
saberma <mahb45@gmail.com>
2009-06-23 21:37:36 +0800

    修正重复发送消息的问题

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  #防止重复提交，参考messages/new.haml
+  def loading_js(submit_btn = 'submit_btn')
+    "j('##{submit_btn}').attr('disabled','disabled').val('提交中...')"
+  end
+
+  def loaded_js(submit_btn = 'submit_btn')
+    "j('##{submit_btn}').removeAttr('disabled').val('提交')"
+  end
 end
