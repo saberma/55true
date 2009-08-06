a191327766c888170eaa58b004ea36fe24140bc2
saberma <mahb45@gmail.com>
2009-08-06 08:40:19 +0800

    缓存首页用户总数

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,17 +3,18 @@ class HomeController < ApplicationController
     #今天谁在玩
     @today_users_size = memcache('today_users_size') { User.today.size }
     @today_users = memcache('today_users') { User.today_top }
+    @users_size = memcache('users_size') { User.all.size }
     #用户问的
     if logged_in?
       @user_unanswer_question_list = Question.unanswer.of current_user
       @user_answered_question_list = Question.limit(2).answered.of(current_user)
       @answer_list = Answer.with_question.paginate(:page => params[:page])
-      flash.now[:notice] = "这里的#{User.all.size}个朋友(共玩了#{@answer_list.total_entries}次)，点击下一行“我敢玩”继续吧!" unless flash[:notice]
+      flash.now[:notice] = "这里的#{@users_size}个朋友(共玩了#{@answer_list.total_entries}次)，点击下一行“我敢玩”继续吧!" unless flash[:notice]
       #用户收到的消息
       @message = Message.to(current_user).first
     else
       @answer_list = Answer.with_question.paginate(:page => params[:page])
-      flash.now[:notice] = "这里的#{User.all.size}个朋友(共玩了#{@answer_list.total_entries}次)，想跟你一起玩真心话，点击下一行“我敢玩”就可以开始了!"
+      flash.now[:notice] = "这里的#{@users_size}个朋友(共玩了#{@answer_list.total_entries}次)，想跟你一起玩真心话，点击下一行“我敢玩”就可以开始了!"
     end
   end
 
