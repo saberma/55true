767ab596b7d61cde485c105cf04f97b7c652fdb2
saberma <mahb45@gmail.com>
2010-12-23 13:06:07 +0800

    10 minute for online timeout.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,9 +5,9 @@ module UsersHelper
     key(Time.now.strftime("%M"))
   end
 
-  def keys_in_last_5_minutes
+  def keys_in_last_minutes
     now = Time.now
-    times = (0..5).collect {|n| now - n.minutes }
+    times = (0..10).collect {|n| now - n.minutes }
     times.collect{ |t| key(t.strftime("%M")) }
   end
 
@@ -20,20 +20,20 @@ module UsersHelper
     key = current_key
     redis.sadd(key, id)
     #redis will reset ttl after modified
-    redis.expire(key, 5.minutes)
+    redis.expire(key, 10.minutes)
   end
 
   # Delete an User
   def lost_user_id(id)
-    keys_in_last_5_minutes.each do |key|
+    keys_in_last_minutes.each do |key|
       redis.srem key, id
-      redis.expire(key, 5.minutes)
+      redis.expire(key, 10.minutes)
     end
   end
 
   # Who's online
   def online_user_ids
-    redis.sunion(*keys_in_last_5_minutes)
+    redis.sunion(*keys_in_last_minutes)
   end
 
   def redis
